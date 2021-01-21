2d152c01ba77e9855320854ec9f7f8d0920fbb2a
Hersain Lozada Hermosa <hersainlozada@gmail.com>
2021-01-20 20:33:57 -0500

    variables

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ vaso="cafe";
 console.log(vaso);
 vaso="agua";
 console.log(vaso);
+
+nombre="miguel";
+edad=23;
+profesor=true;
+lista_cursos=['JavaScript','PHP'];
+informacion_personal=
+{
+    id:123123123,
+    direccion:"CL 2 3 65"
+}
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(lista_cursos);
+console.log(informacion_personal);
